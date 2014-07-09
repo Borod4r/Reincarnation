10030f81d3f6bc521ad715baa4f28842f3b1e3d4
PhannGor <ivgadzhega@gmail.com>
2014-07-09 20:16:52 +0300

    Initial commit

---
+++ desktop/src/net/ivang/reincarnation/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.ivang.reincarnation.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.ivang.reincarnation.ReincarnationGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ReincarnationGame(), config);
+	}
+}
